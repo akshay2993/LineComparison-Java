45ac74eaee82baf9c9b746a6c292109b4f18a51c
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-05-04 11:46:32 +0530

    Successfully added length computation.

--- LineComparison.java
+++ LineComparison.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class LineComparison {
 	
 	public static void main(String[] args) {
-		int x1, x2, y1, y2;
+		int x1, y1, x2, y2;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter coordinates of two points- x1, x2, y1, y2: ");
+		System.out.println("Enter coordinates of two points- x1, y1, x2, y2: ");
 		x1 = sc.nextInt();
-		x2 = sc.nextInt();
 		y1 = sc.nextInt();
+		x2 = sc.nextInt();
 		y2 = sc.nextInt();
-		System.out.println("(x1, x2) - "+ "(" + x1 +", "+ x2  +")\n" + "(y1, y2) - "+ "(" + y1 +", "+ y2  +")");
-
-		double distance = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
-		System.out.println("Distance between (x1, x2) & (y1, y2)/Length: " +distance+" units" );
+		System.out.println("(x1, y1) - "+ "(" + x1 +", "+ y1  +")\n" + "(x2, y2) - "+ "(" + x2 +", "+ y2  +")");
+		double dist = Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2);
+		System.out.println(dist);
+		double res = Math.sqrt(dist);
+		System.out.println("Distance between (x1, x2) & (y1, y2)/Length: " +res+" units" );
 	}
 }
