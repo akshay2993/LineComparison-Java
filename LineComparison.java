29edce512cff34e2ba80c7d36c32fc141a492eb3
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-05-04 10:47:06 +0530

    Successfully added length computation.

--- LineComparison.java
+++ LineComparison.java
@@ -1,8 +1,20 @@
 package com.bridgelabz.javaproblems;
 
+import java.util.Scanner;
+
 public class LineComparison {
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison Computation Program!");
+		int x1, x2, y1, y2;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter coordinates of two points- x1, x2, y1, y2: ");
+		x1 = sc.nextInt();
+		x2 = sc.nextInt();
+		y1 = sc.nextInt();
+		y2 = sc.nextInt();
+		System.out.println("(x1, x2) - "+ "(" + x1 +", "+ x2  +")\n" + "(y1, y2) - "+ "(" + y1 +", "+ y2  +")");
+
+		double distance = Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+		System.out.println("Distance between (x1, x2) & (y1, y2)/Length: " +distance+" units" );
 	}
 }
